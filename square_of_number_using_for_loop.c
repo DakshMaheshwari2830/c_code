3ee758383ee071fac0c1647300204d98948c506d
DakshMaheshwari2830 <dakshmaheshwari2807@gmail.com>
2025-11-18 23:29:38 +0530

    ad files

---
+++ square_of_number_using_for_loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i,n,c;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        c=i*i;
+        printf("%d\n",c);
+    }
+    return 0
+;} 
