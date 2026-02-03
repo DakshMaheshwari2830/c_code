d132c04c3c16947c6994a2d36d51f26bb2982e17
DakshMaheshwari2830 <dakshmaheshwari2807@gmail.com>
2026-02-03 22:41:17 +0530

    add files

---
+++ revrese_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int start = 0, end = n - 1;
+    int temp;
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;}
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
