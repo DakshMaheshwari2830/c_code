8e5da252a44770af4b10fbf7fb83a49ca941421b
DakshMaheshwari2830 <dakshmaheshwari2807@gmail.com>
2026-02-04 23:12:14 +0530

    add files

---
+++ largest_element_in_array.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=5;
+    int max=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
